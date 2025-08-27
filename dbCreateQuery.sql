783bfab53e44e80cf4256ac739b59f44aafda11c
timmillard <timothy.millard@durham.ac.uk>
2025-08-27 10:13:47 +0000

    Updates dbCreateQuery to reflect new schema

--- dbCreateQuery.sql
+++ dbCreateQuery.sql
@@ -1,9 +1,40 @@
-CREATE DATABASE `portfolio` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `PortfolioManager`;
 
+CREATE TABLE `PortfolioManager`.users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 
-CREATE TABLE `assets` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `PortfolioManager`.portfolios (   
+id INT AUTO_INCREMENT PRIMARY KEY,   
+portfolio_name INT NOT NULL,   
+exchange VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `PortfolioManager`.user_portfolios (   
+id INT AUTO_INCREMENT PRIMARY KEY,   
+user_id INT NOT NULL,   
+portfolio_id INT NOT NULL,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (portfolio_id) REFERENCES portfolios(id)
+);
+
+CREATE TABLE `PortfolioManager`.portfolio_assets (   
+id INT AUTO_INCREMENT PRIMARY KEY,   
+portfolio_id INT NOT NULL,
+ticker VARCHAR(255) NOT NULL,
+quantity INT NOT NULL,   
+FOREIGN KEY (portfolio_id) REFERENCES portfolios(id)
+);
+
+CREATE TABLE `PortfolioManager`.transactions (   
+id INT AUTO_INCREMENT PRIMARY KEY,   
+user_id INT NOT NULL,
+portfolio_asset_id INT NOT NULL,
+transaction_type VARCHAR(255) NOT NULL,
+quantity INT NOT NULL,
+datetime DATETIME NOT NULL, 
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (portfolio_asset_id) REFERENCES portfolio_assets(id)
+);
