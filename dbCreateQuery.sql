fc106b0a5a8e190fbefa8a278cc346041b716e72
timmillard <timothy.millard@durham.ac.uk>
2025-08-27 10:30:15 +0000

    Updates database creation query

--- dbCreateQuery.sql
+++ dbCreateQuery.sql
@@ -1,3 +1,7 @@
+-- DO NOT RUN THIS IF YOU HAVE DATA IN THE DATABASE - IT WILL DELETE ALL OF IT.
+
+DROP DATABASE `PortfolioManager`;
+
 CREATE DATABASE `PortfolioManager`;
 
 CREATE TABLE `PortfolioManager`.users (
@@ -8,11 +12,11 @@ CREATE TABLE `PortfolioManager`.users (
 
 CREATE TABLE `PortfolioManager`.portfolios (   
 id INT AUTO_INCREMENT PRIMARY KEY,   
-portfolio_name INT NOT NULL,   
+name INT NOT NULL,   
 exchange VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE `PortfolioManager`.user_portfolios (   
+CREATE TABLE `PortfolioManager`.userPortfolios (   
 id INT AUTO_INCREMENT PRIMARY KEY,   
 user_id INT NOT NULL,   
 portfolio_id INT NOT NULL,
@@ -20,7 +24,7 @@ FOREIGN KEY (user_id) REFERENCES users(id),
 FOREIGN KEY (portfolio_id) REFERENCES portfolios(id)
 );
 
-CREATE TABLE `PortfolioManager`.portfolio_assets (   
+CREATE TABLE `PortfolioManager`.portfolioAssets (   
 id INT AUTO_INCREMENT PRIMARY KEY,   
 portfolio_id INT NOT NULL,
 ticker VARCHAR(255) NOT NULL,
@@ -36,5 +40,5 @@ transaction_type VARCHAR(255) NOT NULL,
 quantity INT NOT NULL,
 datetime DATETIME NOT NULL, 
 FOREIGN KEY (user_id) REFERENCES users(id),
-FOREIGN KEY (portfolio_asset_id) REFERENCES portfolio_assets(id)
+FOREIGN KEY (portfolio_asset_id) REFERENCES portfolioAssets(id)
 );
