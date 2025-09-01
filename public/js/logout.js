16c153f6898523458672f205f6e5433ac256d0ec
timmillard <timothy.millard@durham.ac.uk>
2025-09-01 10:42:58 +0000

    Adds logout button and FE functionality

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,10 +1,5 @@
 document.getElementById("logoutButton").addEventListener("submit", (e) => {
     e.preventDefault();
-
-    
+    localStorage.removeItem("token");
+    window.location.href = "/auth_page.html";
 })
-
-function logout() {
-  localStorage.removeItem("token");
-  window.location.href = "/auth_page.html";
-}
