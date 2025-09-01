8d29f3bb558500ab82a08d095fdf5b05b96b06f0
timmillard <timothy.millard@durham.ac.uk>
2025-09-01 18:47:16 +0000

    Adds purchase price to transaction logic

--- models/transaction.js
+++ models/transaction.js
@@ -87,8 +87,6 @@ async function listAllTransactionsCurrentUser(user_id) {
 
 async function deleteAllByPortfolioAssetId(portfolio_asset_id) {
     try {
-        console.log("Here in deleteTrans")
-
         await Transaction.destroy({
             where: {
               portfolio_asset_id: portfolio_asset_id,
@@ -102,13 +100,14 @@ async function deleteAllByPortfolioAssetId(portfolio_asset_id) {
 
 // change this when Asset contains more attributes
 // delete if we are to add assets manually
-async function addTransaction(user_id, portfolio_id, portfolio_asset_id, transaction_type, quantity) {
+async function addTransaction(userId, portfolioId, portfolioAssetId, transactionType, quantity, purchasePrice) {
     try {
         const newTransaction = await Transaction.create({
-            user_id: user_id,
-            portfolio_id: portfolio_id,
-            portfolio_asset_id: portfolio_asset_id,
-            transaction_type: transaction_type,
+            user_id: userId,
+            portfolio_id: portfolioId,
+            portfolio_asset_id: portfolioAssetId,
+            transaction_type: transactionType,
+            purchase_price: purchasePrice,
             quantity: quantity
         })
         console.log("Transaction Added")
